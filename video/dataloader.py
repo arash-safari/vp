7ae72e1266967d405999dc70bbe848d6248d7d92
unknown <ari@xgdigitalsolutions.com>
2020-05-08 20:49:15 +0430

    add dataset to video

---
+++ video/dataloader.py
@@ -0,0 +1,4 @@
+from torch.utils.data import DataLoader
+
+def video_mnist_dataloader(dataset, n_run, batch_size, shuffle=True, num_workers=4, drop_last=True):
+    return loader
---
+++ video/dataset.py
@@ -0,0 +1,11 @@
+import os
+from torch.utils.data import Dataset
+
+
+class VideoMnistDataset(Dataset):
+    def __init__(self, path):
+        pass
+    def __len__(self):
+        return self.length
+    def __getitem__(self, index):
+        return sample
