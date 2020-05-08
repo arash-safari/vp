c95f04cbd1c8e1bd567ba4d14a819cfd5da4abb5
unknown <ari@xgdigitalsolutions.com>
2020-05-08 23:19:45 +0430

    write dataset and dataloader

--- video/dataloader.py
+++ video/dataloader.py
@@ -1,4 +1,8 @@
 from torch.utils.data import DataLoader
 
-def video_mnist_dataloader(dataset, n_run, batch_size, shuffle=True, num_workers=4, drop_last=True):
+
+def video_mnist_dataloader(dataset, batch_size, shuffle=True, num_workers=4, drop_last=True):
+    loader = DataLoader(
+        dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, drop_last=drop_last
+    )
     return loader
--- video/dataset.py
+++ video/dataset.py
@@ -1,11 +1,23 @@
 import os
 from torch.utils.data import Dataset
+import numpy as np
 
 
 class VideoMnistDataset(Dataset):
-    def __init__(self, path):
-        pass
+    def __init__(self, path, frame_len, start_sample, end_sample):
+        self.frame_len = frame_len
+        self.frames = np.load(path + 'mnist_test_seq.npy')
+        self.frames = self.frames.swapaxes(0, 1)[:, :, start_sample:end_sample, :]
+        frames_shape = self.frames.shape()
+        videos_num = frames_shape[0]
+        video_len = frames_shape[1]
+        self.sample_per_video = video_len - frame_len + 1
+        self.length = videos_num * self.sample_per_video
+
     def __len__(self):
         return self.length
+
     def __getitem__(self, index):
-        return sample
+        video_ind = index / self.sample_per_video
+        frame_ind = index - video_ind * self.sample_per_video
+        return self.frames[video_ind, frame_ind: frame_ind + self.frame_len, :, :]
