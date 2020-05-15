c5f5ac67c73e37be79a00a744c4391d3eeed33fc
unknown <ari@xgdigitalsolutions.com>
2020-05-15 17:17:10 +0430

    debug

--- video/extract_videomnist.py
+++ video/extract_videomnist.py
@@ -3,10 +3,33 @@ import sys
 sys.path.append('../image/modified')
 import torch
 import lmdb
-from image.modified.m_extract_vqvae import extract_videomnist
+from tqdm import tqdm
+from video.dataset import CodeRowVideoMnist
+import pickle
 
 
-def extract(model, ckpt_path, lamda_name, device, loader):
+def extract_videomnist(lmdb_env, loader, model, device):
+    index = 0
+
+    with lmdb_env.begin(write=True) as txn:
+        pbar = tqdm(loader)
+
+        for frame, video_ind, frame_ind in pbar:
+            frame = frame.to(device)
+
+            _, _, _id = model.encode(frame)
+            _id = _id.detach().cpu().numpy()
+
+            for file, _id in zip(_id, frame_ind, video_ind):
+                row = CodeRowVideoMnist(_id=_id, frame_ind=frame_ind, video_ind=video_ind)
+                txn.put(str(index).encode('utf-8'), pickle.dumps(row))
+                index += 1
+                pbar.set_description(f'inserted: {index}')
+
+        txn.put('length'.encode('utf-8'), str(index).encode('utf-8'))
+
+
+def main(model, ckpt_path, lamda_name, device, loader):
     map_size = 100 * 1024 * 1024 * 1024
     model.load_state_dict(torch.load(ckpt_path), strict=False)
     model = model.to(device)
