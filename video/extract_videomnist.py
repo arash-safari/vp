44345d3076d822971df58f80768a1e7006577919
unknown <ari@xgdigitalsolutions.com>
2020-05-15 16:58:46 +0430

    debug

--- video/extract_videomnist.py
+++ video/extract_videomnist.py
@@ -8,7 +8,7 @@ from m_extract_vqvae import extract1
 
 def extract(model, ckpt_path, lamda_name, device, loader):
     map_size = 100 * 1024 * 1024 * 1024
-    model.load_state_dict(torch.load(ckpt_path))
+    model.load_state_dict(torch.load(ckpt_path), strict=False)
     model = model.to(device)
     model.eval()
     env = lmdb.open(lamda_name, map_size=map_size)
