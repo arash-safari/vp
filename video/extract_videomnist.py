00982cf3446b889f5b38b84ebe8b12be56973648
unknown <ari@xgdigitalsolutions.com>
2020-05-15 16:56:56 +0430

    debug

--- video/extract_videomnist.py
+++ video/extract_videomnist.py
@@ -3,10 +3,7 @@ import sys
 sys.path.append('../image/modified')
 import torch
 import lmdb
-from dataset import VideoMnistDataset
-from dataloader import video_mnist_dataloader
 from m_extract_vqvae import extract1
-from m_vqvae import VQVAE_1
 
 
 def extract(model, ckpt_path, lamda_name, device, loader):
@@ -16,21 +13,3 @@ def extract(model, ckpt_path, lamda_name, device, loader):
     model.eval()
     env = lmdb.open(lamda_name, map_size=map_size)
     extract1(env, loader, model, device)
-
-
-def main(model,chkpt_path,lamda_name,batch_size,device,):
-
-# batch_size = 100
-# device = 'cuda'
-    dataset = VideoMnistDataset('datasets/mnist/moving_mnist/mnist_test_seq.npy', 1, 0, 20000)
-    loader = video_mnist_dataloader(dataset, batch_size, shuffle=True, num_workers=4, drop_last=True)
-    extract(model, ckpt_path, lamda_name, device, loader)
-# lamda_name = 'vqvae_videomnist_1'
-# ckpt_path = './checkpoints/videomnist/vqvae/1/1.pt'
-# model = VQVAE_1(in_channel=1,
-#                 channel=32,
-#                 n_res_block=2,
-#                 n_res_channel=16,
-#                 embed_dim=8,
-#                 n_embed=4,
-#                 decay=0.99, )
