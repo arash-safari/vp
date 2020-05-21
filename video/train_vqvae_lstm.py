7d7452a01e8365b747644f06ae2725246a2dcc14
unknown <ari@xgdigitalsolutions.com>
2020-05-21 14:05:05 +0430

    debug lstm

--- video/train_vqvae_lstm.py
+++ video/train_vqvae_lstm.py
@@ -1,13 +1,8 @@
 from torch import nn, optim
-from video.LSTM_VideoMnist import LSTM
-from video.dataset import MnistVideoCodeLMDBDataset
-from video.dataloader import video_mnist_dataloader
-from tqdm import tqdm
+from video.LSTM import LSTM
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
-from torchvision import utils
 import torch
-import numpy as np
 
 
 def get_optimizer(model, lr):
@@ -22,29 +17,16 @@ def _to_one_hot(y, num_classes):
     return zeros.scatter(scatter_dim, y_tensor, 1)
 
 
-def train(vqvae_model):
-    lambda_name = 'vqvae_videomnist_1_00099'
-    input_channel = 8
-    epoch_num = 10
-    batch_size = 64
-    device = 'cuda'
-    lr = 0.0001
-    run_num = 1
+def train(lstm_layers,input_channel, hidden_channel, kernel_size, loader, callback, epoch_num, batch_size, device, lr, run_num, ):
     image_samples = 10
-
-    dataset = MnistVideoCodeLMDBDataset(lambda_name, 3)
-    loader = video_mnist_dataloader(dataset, batch_size, shuffle=True)
-    model = LSTM(2, input_channel, 32, 3)
-    videomnist_path = '../video/datasets/mnist/moving_mnist/mnist_test_seq.npy'
-    orginal_frames = np.load(videomnist_path)
-    orginal_frames = orginal_frames.swapaxes(0, 1).astype(np.float32)
-    orginal_frames[orginal_frames > 0] = 1.
-
+    model = LSTM(lstm_layers, input_channel, hidden_channel, kernel_size)
+    writer_path = 'vqvae_videomnist_1_00099_lstm'
     optimizer = get_optimizer(model, lr)
     model = model.to(device)
     model = nn.DataParallel(model)
     criterion = nn.MSELoss()
-    writer = SummaryWriter(log_dir='logs/{}_{}'.format(*['vqvae_videomnist_1_00099_lstm', run_num]))
+
+    writer = SummaryWriter(log_dir='logs/{}_{}'.format(*[writer_path, run_num]))
 
     for epoch in range(epoch_num):
         loader = tqdm(loader)
@@ -75,18 +57,8 @@ def train(vqvae_model):
                 writer.add_scalar('Loss/train', mse_sum / mse_n, epoch_num)
                 model.eval()
                 sample = pred[0, :image_samples, :, :]
-                o_frames = orginal_frames[video_inds[0], :image_samples, :, :]
-                with torch.no_grad():
-                    sample = vqvae_model.module.decode_code(sample)
-
-                    utils.save_image(
-                        torch.cat([sample, o_frames], 0),
-                        dir + 'samples/videomnist/vqvae/{}/{}.png'.format(*[run_num, epoch]),
-                        nrow=image_samples,
-                        normalize=True,
-                        range=(-1, 1),
-                    )
-                    model.train()
+                callback(sample)
+                model.train()
 
             torch.save(model.state_dict(),
                        dir + 'checkpoints/videomnist/vqvae-lstm/{}/{}.pt'.format(*[run_num, str(epoch).zfill(5)]))
