2622e573fc62ea96c62a26718481523ef6f0b1d8
unknown <ari@xgdigitalsolutions.com>
2020-05-24 12:27:28 +0430

    debug

--- video/LSTM.py
+++ video/LSTM.py
@@ -12,22 +12,19 @@ class LSTM(nn.Module):
         self.hidden_channel = hidden_channel
         self.kernel_size = kernel_size
         self.cells = nn.ModuleList()
-        self.cells_state = []
         self.device = device
         for i in range(layers_num):
             cell = ConvLstmCell(input_channel,hidden_channel,kernel_size, device)
             self.cells.append(cell)
-            self.cells_state.append(None)
 
     def forward(self, input, cells_state = None):
         input_ = input
-        if not cells_state is None:
-            self.cells_state = cells_state
-
+        if cells_state is None:
+            cells_state = [None] * len(self.cells)
         for i, cell in enumerate(self.cells):
-            self.cells_state[i] = cell(input_, self.cells_state[i])
-            hidden, cell = self.cells_state[i]
+            cells_state[i] = cell(input_, cells_state[i])
+            hidden, cell = cells_state[i]
             input_ = hidden
 
-        return self.cells_state
+        return cells_state
 
