a993104290ecd804cd6b16e5bec2846d21e681d2
unknown <ari@xgdigitalsolutions.com>
2020-06-05 16:37:38 +0430

    debug

--- image/vqvae.py
+++ image/vqvae.py
@@ -38,11 +38,16 @@ class Quantize(nn.Module):
 
     def forward(self, input):
         flatten = input.reshape(-1, self.dim)
+        print('flatten input {}'.format(input))
+        print('quantize embed {}'.format(self.embed))
+
         dist = (
             flatten.pow(2).sum(1, keepdim=True)
             - 2 * flatten @ self.embed
             + self.embed.pow(2).sum(0, keepdim=True)
         )
+        print('quantize dist {}'.format(dist.shape))
+
         _, embed_ind = (-dist).max(1)
         embed_onehot = F.one_hot(embed_ind, self.n_embed).type(flatten.dtype)
         embed_ind = embed_ind.view(*input.shape[:-1])
