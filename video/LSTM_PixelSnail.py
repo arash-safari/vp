196c87fa9392404b794490b11a2ec17c8b2b8c9a
unknown <ari@xgdigitalsolutions.com>
2020-06-08 00:46:07 +0430

    debug

--- video/LSTM_PixelSnail.py
+++ video/LSTM_PixelSnail.py
@@ -1,6 +1,7 @@
 from torch import nn
 from video.ConvLstmCell import ConvLstmCell
-import torch.nn.functional as F
+import torch
+from tqdm import tqdm
 
 
 class LSTM_PixelSnail(nn.Module):
@@ -20,3 +21,20 @@ class LSTM_PixelSnail(nn.Module):
         cnn_out = self.cnn_model(lstm_out)
         out, _ = self.pixel_model(target, condition=cnn_out)
         return out, cells_state
+
+    @torch.no_grad()
+    def sample_model(self, input_, cells_state, device, batch, size, temperature, condition=None):
+        row = torch.zeros(batch, *size, dtype=torch.int64).to(device)
+        cache = {}
+        lstm_out, cells_state = self.lstm_model(input_, cells_state)
+        cnn_out = self.cnn_model(lstm_out)
+        size = input_.size()
+
+        for i in tqdm(range(size[0])):
+            for j in range(size[1]):
+                out, cache = self.pixel_model(row[:, : i + 1, :], condition=cnn_out, cache=cache)
+                prob = torch.softmax(out[:, :, i, j] / temperature, 1)
+                sample = torch.multinomial(prob, 1).squeeze(-1)
+                row[:, i, j] = sample
+
+        return row, cells_state
