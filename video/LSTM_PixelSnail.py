76aa8d3bdb335718522b20dc05da159247458ef7
unknown <ari@xgdigitalsolutions.com>
2020-06-10 00:50:04 +0430

    debug

--- video/LSTM_PixelSnail.py
+++ video/LSTM_PixelSnail.py
@@ -43,7 +43,7 @@ class LSTM_PixelSnail(nn.Module):
                 prob = torch.softmax(out[:, :, i, j] / temperature, 1)
                 # print(prob.size())
                 sample = torch.multinomial(prob, 1).squeeze(-1)
-                print('befor one hot {}'.format(sample.size))
+                print('befor one hot {}'.format(sample.size()))
                 sample = self._to_one_hot(sample, size[1]).float()
                 print('after one hot {}'.format(sample.size()))
 
