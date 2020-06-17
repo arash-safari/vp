6cf9b20c184d3b81265f61c9a2278e3f5174d359
unknown <ari@xgdigitalsolutions.com>
2020-06-18 03:07:49 +0430

    debug

--- video/train_vqvae_kth_breakfast.py
+++ video/train_vqvae_kth_breakfast.py
@@ -32,48 +32,48 @@ def train(model, epoch_num, batch_size, lr, device, run_num, image_samples):
         loader = tqdm(loader)
 
         for iter, video in enumerate(loader):
-            for img in video:
-                model.zero_grad()
-                print(img.shape)
-                img = img.to(device)
-                out, latent_loss = model(img)
-                recon_loss = criterion(out, img)
-                latent_loss = latent_loss.mean()
-                loss = recon_loss + latent_loss_weight * latent_loss
-                loss.backward()
+            # for img in video:
+            video = video.permute(0,3,1,2)
+            model.zero_grad()
+            video = video.to(device)
+            out, latent_loss = model(video)
+            recon_loss = criterion(out, video)
+            latent_loss = latent_loss.mean()
+            loss = recon_loss + latent_loss_weight * latent_loss
+            loss.backward()
 
-                optimizer.step()
+            optimizer.step()
 
-                mse_sum += recon_loss.item() * img.shape[0]
-                mse_n += img.shape[0]
+            mse_sum += recon_loss.item() * video.shape[0]
+            mse_n += video.shape[0]
 
-                lr = optimizer.param_groups[0]['lr']
-                if iter % 200 is 0:
-                    loader.set_description(
-                        (
-                            'iter: {iter + 1}; mse: {recon_loss.item():.5f}; '
-                            f'latent: {latent_loss.item():.3f}; avg mse: {mse_sum / mse_n:.5f}; '
-                            f'lr: {lr:.5f}'
-                        )
+            lr = optimizer.param_groups[0]['lr']
+            if iter % 200 is 0:
+                loader.set_description(
+                    (
+                        'iter: {iter + 1}; mse: {recon_loss.item():.5f}; '
+                        f'latent: {latent_loss.item():.3f}; avg mse: {mse_sum / mse_n:.5f}; '
+                        f'lr: {lr:.5f}'
                     )
-                if iter is 0 and epoch > 0:
-                    writer.add_scalar('Loss/train', mse_sum / mse_n, epoch_num)
-                    model.eval()
-                    sample = img[:image_samples]
+                )
+            if iter is 0 and epoch > 0:
+                writer.add_scalar('Loss/train', mse_sum / mse_n, epoch_num)
+                model.eval()
+                sample = video[:image_samples]
 
-                    with torch.no_grad():
-                        out, _ = model(sample)
-                    # save samples to see result
+                with torch.no_grad():
+                    out, _ = model(sample)
+                # save samples to see result
 
-                    out = (out > 0.5).float()
-                    utils.save_image(
-                        torch.cat([sample, out], 0),
-                        dir + 'samples/kth-breakfast/vqvae/{}/{}.png'.format(*[run_num, epoch]),
-                        nrow=image_samples,
-                        normalize=True,
-                        range=(-1, 1),
-                    )
-                    model.train()
+                out = (out > 0.5).float()
+                utils.save_image(
+                    torch.cat([sample, out], 0),
+                    dir + 'samples/kth-breakfast/vqvae/{}/{}.png'.format(*[run_num, epoch]),
+                    nrow=image_samples,
+                    normalize=True,
+                    range=(-1, 1),
+                )
+                model.train()
 
             torch.save(model.state_dict(),
                        dir + 'checkpoints/kth-breakfast/vqvae/{}/{}.pt'.format(*[run_num, str(epoch).zfill(5)]))
