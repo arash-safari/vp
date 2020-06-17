fb07c5c840862e27e569fab47d76b2d141ead8a9
unknown <ari@xgdigitalsolutions.com>
2020-06-17 18:50:49 +0430

    debug

--- video/train_vqvae_kth_breakfast.py
+++ video/train_vqvae_kth_breakfast.py
@@ -5,7 +5,7 @@ from m_vqvae import VQVAE_1
 from torch import optim, nn
 import torch
 from torchvision import utils
-from dataset import Kth_Breakfast_VideoDataset
+from dataset import Hdf5Dataset
 from dataloader import video_mnist_dataloader
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
@@ -17,7 +17,7 @@ def get_optimizer(model, lr):
 
 def train(model, epoch_num, batch_size, lr, device, run_num, image_samples):
     dir = '/home/stipendiater/mohamadi/vp/video/'
-    dataset = Kth_Breakfast_VideoDataset(dir + 'datasets/kth', 1)
+    dataset = Hdf5Dataset(dir + 'datasets/kth_breakfast.h5', 'train')
     loader = video_mnist_dataloader(dataset, batch_size, shuffle=True, num_workers=4, drop_last=True)
     optimizer = get_optimizer(model, lr)
     model = model.to(device)
@@ -31,48 +31,49 @@ def train(model, epoch_num, batch_size, lr, device, run_num, image_samples):
         mse_n = 0
         loader = tqdm(loader)
 
-        for iter, img in enumerate(loader):
-            model.zero_grad()
-            img = img[0]
-            img = img.to(device)
-            out, latent_loss = model(img)
-            recon_loss = criterion(out, img)
-            latent_loss = latent_loss.mean()
-            loss = recon_loss + latent_loss_weight * latent_loss
-            loss.backward()
+        for iter, video in enumerate(loader):
+            for img in video:
+                model.zero_grad()
+                img = img[0]
+                img = img.to(device)
+                out, latent_loss = model(img)
+                recon_loss = criterion(out, img)
+                latent_loss = latent_loss.mean()
+                loss = recon_loss + latent_loss_weight * latent_loss
+                loss.backward()
 
-            optimizer.step()
+                optimizer.step()
 
-            mse_sum += recon_loss.item() * img.shape[0]
-            mse_n += img.shape[0]
+                mse_sum += recon_loss.item() * img.shape[0]
+                mse_n += img.shape[0]
 
-            lr = optimizer.param_groups[0]['lr']
-            if iter % 200 is 0:
-                loader.set_description(
-                    (
-                        'iter: {iter + 1}; mse: {recon_loss.item():.5f}; '
-                        f'latent: {latent_loss.item():.3f}; avg mse: {mse_sum / mse_n:.5f}; '
-                        f'lr: {lr:.5f}'
+                lr = optimizer.param_groups[0]['lr']
+                if iter % 200 is 0:
+                    loader.set_description(
+                        (
+                            'iter: {iter + 1}; mse: {recon_loss.item():.5f}; '
+                            f'latent: {latent_loss.item():.3f}; avg mse: {mse_sum / mse_n:.5f}; '
+                            f'lr: {lr:.5f}'
+                        )
                     )
-                )
-            if iter is 0 and epoch > 0:
-                writer.add_scalar('Loss/train', mse_sum / mse_n, epoch_num)
-                model.eval()
-                sample = img[:image_samples]
+                if iter is 0 and epoch > 0:
+                    writer.add_scalar('Loss/train', mse_sum / mse_n, epoch_num)
+                    model.eval()
+                    sample = img[:image_samples]
 
-                with torch.no_grad():
-                    out, _ = model(sample)
-                # save samples to see result
+                    with torch.no_grad():
+                        out, _ = model(sample)
+                    # save samples to see result
 
-                out = (out > 0.5).float()
-                utils.save_image(
-                    torch.cat([sample, out], 0),
-                    dir + 'samples/kth-breakfast/vqvae/{}/{}.png'.format(*[run_num, epoch]),
-                    nrow=image_samples,
-                    normalize=True,
-                    range=(-1, 1),
-                )
-                model.train()
+                    out = (out > 0.5).float()
+                    utils.save_image(
+                        torch.cat([sample, out], 0),
+                        dir + 'samples/kth-breakfast/vqvae/{}/{}.png'.format(*[run_num, epoch]),
+                        nrow=image_samples,
+                        normalize=True,
+                        range=(-1, 1),
+                    )
+                    model.train()
 
-        torch.save(model.state_dict(),
-                   dir + 'checkpoints/kth-breakfast/vqvae/{}/{}.pt'.format(*[run_num, str(epoch).zfill(5)]))
+            torch.save(model.state_dict(),
+                       dir + 'checkpoints/kth-breakfast/vqvae/{}/{}.pt'.format(*[run_num, str(epoch).zfill(5)]))
