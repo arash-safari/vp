349b78e71d94372bcea86f7ff21e64c4a4156fa0
unknown <ari@xgdigitalsolutions.com>
2020-09-10 13:23:34 +0330

    debug

--- video/dataset.py
+++ video/dataset.py
@@ -34,25 +34,25 @@ class MnistVideoDataset(Dataset):
         return self.frames[video_ind, frame_ind: frame_ind + self.frame_len, :, :], video_ind, frame_ind
 
 
-class MnistVideoDataset2(Dataset):
-    def __init__(self, path, frame_len):
-        self.frame_len = int(frame_len)
-        self.frames = np.load(path)
-        self.frames = self.frames.swapaxes(0, 1).astype(np.float32)
-        self.frames[self.frames > 0] = 1.
-        frames_shape = self.frames.shape
-        videos_num = frames_shape[0]
-        video_len = frames_shape[1]
-        self.sample_per_video = video_len - frame_len + 1
-        self.length = (videos_num * self.sample_per_video * (self.sample_per_video -1) )/2
-
-    def __len__(self):
-        return self.length
-
-    def __getitem__(self, index):
-        video_ind = int(2 * index / (self.sample_per_video*(self.sample_per_video - 1)))
-        frame_ind = index - video_ind * (self.sample_per_video*(self.sample_per_video - 1))
-        return self.frames[video_ind, frame_ind: min(frame_ind + self.frame_len, self.frames.shape[1]), :, :], video_ind, frame_ind
+# class MnistVideoDataset2(Dataset):
+#     def __init__(self, path, frame_len):
+#         self.frame_len = int(frame_len)
+#         self.frames = np.load(path)
+#         self.frames = self.frames.swapaxes(0, 1).astype(np.float32)
+#         self.frames[self.frames > 0] = 1.
+#         frames_shape = self.frames.shape
+#         videos_num = frames_shape[0]
+#         video_len = frames_shape[1]
+#         self.sample_per_video = video_len - frame_len + 1
+#         self.length = (videos_num * self.sample_per_video * (self.sample_per_video -1) )/2
+#
+#     def __len__(self):
+#         return self.length
+#
+#     def __getitem__(self, index):
+#         video_ind = int(2 * index / (self.sample_per_video*(self.sample_per_video - 1)))
+#         frame_ind = index - video_ind * (self.sample_per_video*(self.sample_per_video - 1))
+#         return self.frames[video_ind, frame_ind: min(frame_ind + self.frame_len, self.frames.shape[1]), :, :], video_ind, frame_ind
 
 class lmdb_video(Dataset):
     def __init__(self, env_path, env_meta):
@@ -84,7 +84,7 @@ class lmdb_video(Dataset):
             raise IOError('Cannot open lmdb dataset', env_meta)
 
         with meta.begin(write=False) as txn:
-            self.videos_ind  = pickle.loads(txn.get('videos_ind'.encode('utf-8')).decode('utf-8'))
+            self.videos_ind = pickle.loads(txn.get('videos_ind'.encode('utf-8')).decode('utf-8'))
             self.frames_ind = pickle.loads(txn.get('frames_ind'.encode('utf-8')).decode('utf-8'))
 
     def __len__(self):
@@ -96,8 +96,7 @@ class lmdb_video(Dataset):
             self.group = file[self.group_name]
             # print(f"vid_part_{i}")
             # print(np.array(self.group[f"vid_part_{i}"]).shape)
-        return   np.array(self.group[f"vid_part_{i}"]).astype(np.float)
-
+        return np.array(self.group[f"vid_part_{i}"]).astype(np.float)
 
 
 class MnistVideoCodeLMDBDataset(Dataset):
@@ -133,3 +132,38 @@ class MnistVideoCodeLMDBDataset(Dataset):
             row = pickle.loads(txn.get(key))
 
         return torch.from_numpy(row.ids[frame_ind: frame_ind + self.frame_len]), row.video_ind, frame_ind
+
+
+class MnistVideoCodeLMDBDataset2(Dataset):
+    def __init__(self, path, frame_len):
+        self.env = lmdb.open(
+            path,
+            max_readers=32,
+            readonly=True,
+            lock=False,
+            readahead=False,
+            meminit=False,
+        )
+        self.frame_len = int(frame_len)
+        video_len = 20
+        self.sample_per_video = video_len - frame_len + 1
+
+        if not self.env:
+            raise IOError('Cannot open lmdb dataset', path)
+
+        with self.env.begin(write=False) as txn:
+            self.length = int(txn.get('length'.encode('utf-8')).decode('utf-8')) * (self.sample_per_video* (self.sample_per_video -1) )/2
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, index):
+        with self.env.begin(write=False) as txn:
+            video_ind = int(index / self.sample_per_video)
+            frame_ind = index - video_ind * (self.sample_per_video* (self.sample_per_video -1) )/2
+
+            key = str(video_ind).encode('utf-8')
+
+            row = pickle.loads(txn.get(key))
+
+        return torch.from_numpy(row.ids[frame_ind: min(frame_ind + self.frame_len,row.ids.shape[0])]), row.video_ind, frame_ind
